ad32218ac9be05b42a79a0ede5cfc8085248d99e
projectdanz <akunprojectdanz@gmail.com>
2025-05-23 10:38:53 +0700

    feat(users): penambahan kolom role di table users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,11 +23,13 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $role = ['admin', 'kasir', 'user'];
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'role' => fake()->randomElement($role),
             'remember_token' => Str::random(10),
         ];
     }
